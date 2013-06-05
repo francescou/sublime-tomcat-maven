4d795614ce127b4cdc17430629123a220db5753b
Francesco Uliana <francesco@uliana.it>
2013-06-05 17:49:28 +0200

    os independent separator
    os.path.join

--- cool.py
+++ cool.py
@@ -1,4 +1,4 @@
-import sublime, sublime_plugin, shutil, re
+import sublime, sublime_plugin, shutil, re, os
 
 project = 'cool-common'
 
@@ -6,5 +6,6 @@ class CoolListener(sublime_plugin.EventListener):
   def on_post_save(self, view):
     path = view.file_name()
     if (project in path):
-        dest = re.sub(r'^(.*' + project + ').*(META\-INF.*)$', r'\1\\target\\classes\\\2', path)
+        grps = re.match(r'^(.*' + project + ').*(META\-INF.*)$', path)
+        dest = os.path.join(grps.group(1), 'target', 'classes', grps.group(2))
         shutil.copy2(path, dest)
